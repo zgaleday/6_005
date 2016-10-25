415e6e855ad60ef2be8ca0e71d9db50f779d6375
zgaleday <zgd3200@gmail.com>
2016-10-24 21:42:40 -0400

    Add test cases for guessFollowsGraph in FilterTest

--- ps2/test/twitter/SocialNetworkTest.java
+++ ps2/test/twitter/SocialNetworkTest.java
@@ -2,7 +2,9 @@ package twitter;
 
 import static org.junit.Assert.*;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,13 +15,90 @@ import org.junit.Test;
 public class SocialNetworkTest {
 
     /*
-     * TODO: your testing strategies for these methods should go here.
-     * Make sure you have partitions.
+     * Partions for guessFollowsGraph:
+     * tweets: not mutated by method, empty
+     * Author: no follows either empty set or not in map, different casing in Author doesn't give two entries
+     * Follows:can't follow self, not case sensitive
+     * @mentions: if user @ mentions follow in map. Test across multiple tweets.
+     * same user different cases. Multiple @ one tweet.(don't stop parse after self mention)
      */
+    
+    
+    private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
+    private static final Instant d2 = Instant.parse("2016-02-17T11:00:00Z");
+    
+    private static final Tweet tweet1 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
+    private static final Tweet tweet2 = new Tweet(2, "bbitdiddle", "rivest talk in 30 minutes #hype", d2);
+    private static final Tweet tweet3 = new Tweet(3, "alyssa", "", d1);
+    private static final Tweet tweet4 = new Tweet(4, "alyssa", "@alyssa", d1);
+    private static final Tweet tweet5 = new Tweet(5, "alyssa", "@bbitdiddle", d1);
+    private static final Tweet tweet6 = new Tweet(6, "alyssa", "@Bbitdiddle", d1);
+    private static final Tweet tweet7 = new Tweet(7, "Alyssa", "@bbitdiddle", d1);
+    private static final Tweet tweet8 = new Tweet(8, "alyssa", "@alyssa @bbitdiddle @coolcat", d1);
+    private static final Tweet tweet9 = new Tweet(9, "alyssa", "@popping", d1);
+    
     
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+    
+    @Test
+    public void testGuessFollowsGraphNoInfo() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet3));
+        
+        if (followsGraph.containsKey("alyssa"))
+            assertEquals("author supposed to have no follows", 0, followsGraph.get("alyssa").size());
+        else
+            assertTrue("expected empty graph", followsGraph.isEmpty());
+    }
+    
+    @Test
+    public void testGuessFollowsGraphFollowCase() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet5, tweet6));
+        
+        for (String user : followsGraph.keySet())
+            if (user.toLowerCase().equals("alyssa"))
+                assertEquals("expected one follow", 1, followsGraph.get(user).size());
+        assertEquals("user not in map", 0, 1);
+    }
+    
+    @Test
+    public void testGuessFollowsGraphMultipleAt() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet8, tweet9));
+        
+        for (String user : followsGraph.keySet())
+            if (user.toLowerCase().equals("alyssa"))
+                assertEquals("expected one follow", 3, followsGraph.get(user).size());
+        assertEquals("user not in map", 0, 1);
+    }
+    
+    @Test
+    public void testGuessFollowsGraphAuthorCase() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet5, tweet7));
+        
+        assertEquals("expected one follow", 1, followsGraph.size());
+    }
+    
+    @Test
+    public void testGuessFollowsSelfAt() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet4));
+        
+        if (followsGraph.containsKey("alyssa"))
+            assertEquals("author supposed to have no follows", 0, followsGraph.get("alyssa").size());
+        else
+            assertTrue("expected empty graph", followsGraph.isEmpty());
+    }
+    
+    @Test
+    public void testGuessFollowsNotMutateList() {
+        Tweet[] tweets = {tweet1, tweet2};
+        Tweet[] tweets2 = {tweet1, tweet2};
+        
+        assertTrue("equals", Arrays.equals(tweets, tweets2));
+        SocialNetwork.guessFollowsGraph(Arrays.asList(tweets2));
+        
+        assertTrue("immutable", Arrays.equals(tweets, tweets2)); //Ensure list immutable
     }
     
     @Test
