1e7a09cd568eb8b5724a04809b5896dbd014d64b
zgaleday <zgd3200@gmail.com>
2016-11-27 10:25:48 -0500

    Add test for condition safety from rep exposure.

--- ps3/test/library/BookCopyTest.java
+++ ps3/test/library/BookCopyTest.java
@@ -50,6 +50,17 @@ public class BookCopyTest {
         assertEquals("Condition doesn't match after setCondition",BookCopy.Condition.DAMAGED, copy.getCondition());
     }
     
+    @Test
+    public void testGetCondition() {
+        Book book = new Book("This Test Is Just An Example", Arrays.asList("You Should", "Replace It", "With Your Own Tests"), 1990);
+        BookCopy copy = new BookCopy(book);
+        assertEquals("Initial condition not GOOD",BookCopy.Condition.GOOD, copy.getCondition());
+        copy.setCondition(BookCopy.Condition.DAMAGED);
+        BookCopy.Condition condition = copy.getCondition();
+        condition = BookCopy.Condition.GOOD;
+        assertEquals("Condition doesn't match after setCondition",BookCopy.Condition.DAMAGED, copy.getCondition());
+    }
+    
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
